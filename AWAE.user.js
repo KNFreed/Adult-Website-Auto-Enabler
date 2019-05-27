427858291ccd30420f113c70bf3604f2f51a9acc
Frédéric Spataro <44784500+KNFreed@users.noreply.github.com>
2019-05-27 16:25:41 +0200

    Damn it works
    Im lucky

--- AWAE.user.js
+++ AWAE.user.js
@@ -3,9 +3,7 @@
 // @version      1.3
 // @description  Automatically gives adult permission on many websites
 // @author       Freed
-// @match        http*://*.toranoana.jp/*
-// @include        http*://www.melonbooks.co.jp/*
-// @include        http*://www.suruga-ya.jp/*
+// @match        *://*/*
 // @updateURL    https://github.com/KNFreed/Adult-Website-Auto-Enabler/raw/master/AWAE.user.js
 // ==/UserScript==
 
@@ -24,26 +22,30 @@ function getCookie(cname) {
   return "";
 }
 
+var currLoc = window.location.hostname
+
 // Adults Shop websites
 
-if (currLoc == "http*://*.toranoana.jp/*") {
+if (currLoc.includes("toranoana.jp")) {
     var adult = getCookie("adflg");
       if (adult != "0") {
         document.cookie = "adflg=0";
          location.reload();
 }
+}
 
-if (currLoc == "http*://www.melonbooks.co.jp/*") {
+if (currLoc.includes("melonbooks.co.jp")) {
     var adult = getCookie("AUTH_ADULT");
       if (adult != "1") {
         document.cookie = "AUTH_ADULT=1";
          location.reload();
 }
+}
 
-if (currLoc == "http*://www.suruga-ya.jp/*") {
+if (currLoc.includes("suruga-ya.jp")) {
     var adult = getCookie("adult");
       if (adult != "1") {
-        document.cookie = "Aadult=1";
+        document.cookie = "adult=1";
          location.reload();
 }
 }
